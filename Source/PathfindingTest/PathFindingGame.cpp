f7be3e8eeda0a300f04894faa9ca9fee93206514
jonathansty <jonathansty@users.noreply.github.com>
2022-07-16 22:34:15 +0100

    Add quick prototype for basic path finding algorithm

--- Source/PathfindingTest/PathFindingGame.cpp
+++ Source/PathfindingTest/PathFindingGame.cpp
@@ -2,6 +2,46 @@
 #include "PathFindingGame.h"
 #include "InputManager.h"
 
+#include <queue>
+
+
+
+
+NavPathGrid construct_grid_from_pos(NavGrid& grid, u32 x0, u32 y0)
+{
+	NavPathGrid temp = NavPathGrid::from(grid);
+	u32 width = grid.m_Width;
+	u32 idx = x0 + y0 * width;
+
+	// Add start to frontier
+	std::queue<NavCell*> frontier;
+	std::set<NavCell*> reached;
+	frontier.push(&temp._cells[idx]);
+	reached.insert(&temp._cells[idx]);
+
+	while (!frontier.empty())
+	{
+		// Get the first in the frontier queue
+		NavCell* current = frontier.front();
+		frontier.pop();
+
+		// Retrieve all neighbours and, if neighbour not in reached consider this next iteration in frontier
+		std::vector<NavCell*> const& neighbours = current->neighbours;
+		for (NavCell* n : neighbours)
+		{
+			if (n->passable && reached.find(n) == reached.end())
+			{
+				n->previous = current;
+
+				frontier.push(n);
+				reached.insert(n);
+			}
+		}
+	}
+
+	return temp;
+}
+
 void PathFindingGame::configure_engine(EngineSettings& settings)
 {
 	settings.d2d_use = true;
@@ -29,27 +69,31 @@ void PathFindingGame::start(void)
 		int x = i % m_Grid.m_Width;
 		int y = i / m_Grid.m_Height; 
 
+		m_Grid.m_GridCells[i].idx = i;
 		m_Grid.m_GridCells[i].centre = float2{ x*m_Grid.m_CellSize, y*m_Grid.m_CellSize };
 	}
 
-	m_Start = &get_cell(m_Grid, 0, 0);
-	m_End = &get_cell(m_Grid, 5, 8);
+	// Setup the grid
+	get_cell(m_Grid, 0, 3)->passable = false;
+	get_cell(m_Grid, 1, 3)->passable = false;
+	get_cell(m_Grid, 2, 3)->passable = false;
+	get_cell(m_Grid, 3, 3)->passable = false;
+	get_cell(m_Grid, 4, 3)->passable = false;
+	get_cell(m_Grid, 5, 3)->passable = false;
+	get_cell(m_Grid, 6, 3)->passable = false;
 
-	get_cell(m_Grid, 0, 3).passable = false;
-	get_cell(m_Grid, 1, 3).passable = false;
-	get_cell(m_Grid, 2, 3).passable = false;
-	get_cell(m_Grid, 3, 3).passable = false;
-	get_cell(m_Grid, 4, 3).passable = false;
-	get_cell(m_Grid, 5, 3).passable = false;
-	get_cell(m_Grid, 6, 3).passable = false;
+	get_cell(m_Grid, 3, 6)->passable = false;
+	get_cell(m_Grid, 4, 6)->passable = false;
+	get_cell(m_Grid, 5, 6)->passable = false;
+	get_cell(m_Grid, 6, 6)->passable = false;
+	get_cell(m_Grid, 8, 6)->passable = false;
+	get_cell(m_Grid, 7, 6)->passable = false;
+	get_cell(m_Grid, 9, 6)->passable = false;
 
-	get_cell(m_Grid, 3, 6).passable = false;
-	get_cell(m_Grid, 4, 6).passable = false;
-	get_cell(m_Grid, 5, 6).passable = false;
-	get_cell(m_Grid, 6, 6).passable = false;
-	get_cell(m_Grid, 8, 6).passable = false;
-	get_cell(m_Grid, 7, 6).passable = false;
-	get_cell(m_Grid, 9, 6).passable = false;
+	m_NavigationGrid = construct_grid_from_pos(m_Grid, m_Start.x, m_Start.y);
+
+	m_Start = float2(0.0);
+	m_End = float2(9, 9);
 
 	m_ViewTranslation = float2(0.0, 0.0);
 }
@@ -61,48 +105,93 @@ void PathFindingGame::end(void)
 void PathFindingGame::paint(Graphics::D2DRenderContext& ctx)
 {
 	
-	float3x3 view = float3x3::translation(-m_ViewTranslation);
+	float3x3 zoom = float3x3::scale(m_ViewZoom);
+	auto engine = GameEngine::instance();
+	float3x3 view = hlslpp::mul(float3x3::translation(-m_ViewTranslation), zoom);
 	ctx.set_view_matrix(view);
 	ctx.draw_background(MK_COLOR(33, 33, 33, 255));
 
 	ctx.set_color(MK_COLOR(0, 255, 0, 255));
 	// Paint the navigation grid
-
 	for (size_t i = 0; i < m_Grid.m_GridCells.size(); ++i)
 	{
 		NavGridCell const& cell = m_Grid.m_GridCells[i];
+		NavCell const& nav_cell = m_NavigationGrid._cells[i];
+
 		ctx.fill_ellipse(cell.centre, 1.0, 1.0);
 		if (cell.passable)
 		{
-			ctx.draw_ellipse(cell.centre, m_Grid.m_CellSize * 0.5f, m_Grid.m_CellSize * 0.5f);
+			//ctx.draw_ellipse(cell.centre, m_Grid.m_CellSize * 0.5f, m_Grid.m_CellSize * 0.5f);
+			ctx.draw_rect(cell.centre - float2(m_Grid.m_CellSize * 0.5f), cell.centre + float2(m_Grid.m_CellSize * 0.5f));
 		}
 		else
 		{
-			ctx.fill_ellipse(cell.centre, m_Grid.m_CellSize * 0.5f, m_Grid.m_CellSize * 0.5f);
+			//ctx.fill_ellipse(cell.centre, m_Grid.m_CellSize * 0.5f, m_Grid.m_CellSize * 0.5f);
+			ctx.fill_rect(cell.centre - float2(m_Grid.m_CellSize * 0.5f), cell.centre + float2(m_Grid.m_CellSize * 0.5f));
 		}
+
+		if(nav_cell.previous != nullptr)
+		{
+			// Point at the previous cell
+			constexpr f32 line_length = 25.0f;
+
+			u32 previous_index = m_NavigationGrid.get_cell_idx(nav_cell.previous);
+			float2 direction = m_Grid.m_GridCells[previous_index].centre - cell.centre;
+			direction = hlslpp::normalize(direction);
+			ctx.draw_line(cell.centre, cell.centre + line_length * direction , 0.2f);
+		}
+	}
+
+	ctx.set_color(MK_COLOR(255, 255, 0, 255));
+
+	NavCell* result = m_NavigationGrid.find_path(m_End.x, m_End.y);
+	while(result)
+	{
+		u32 idx = m_NavigationGrid.get_cell_idx(result);
+		NavGridCell const& cell = m_Grid.m_GridCells[idx];
+
+		// Draw line
+		if(result->previous)
+		{
+			u32 prev_idx = m_NavigationGrid.get_cell_idx(result->previous);
+			NavGridCell const& prev_cell = m_Grid.m_GridCells[prev_idx];
+			ctx.draw_line(cell.centre, prev_cell.centre, 0.5f);
+		}
+
+		result = result->previous;
 	}
 
 
 	ctx.set_color(MK_COLOR(255, 0, 0, 255));
-	if (m_Start)
+	if (NavGridCell* cell = get_cell(m_Grid, m_Start.x, m_Start.y); cell)
 	{
-		NavGridCell const& cell = *m_Start;
-		ctx.fill_ellipse(cell.centre, 1.0, 1.0);
-		ctx.draw_ellipse(cell.centre, m_Grid.m_CellSize * 0.5f, m_Grid.m_CellSize * 0.5f);
+		ctx.fill_ellipse(cell->centre, 1.0, 1.0);
+		ctx.draw_ellipse(cell->centre, m_Grid.m_CellSize * 0.5f, m_Grid.m_CellSize * 0.5f);
 	}
 
 	ctx.set_color(MK_COLOR(0, 0, 255, 255));
-	if (m_End)
+
+	if (NavGridCell* cell = get_cell(m_Grid, m_Start.x, m_Start.y); cell)
 	{
-		NavGridCell const& cell = *m_End;
-		ctx.fill_ellipse(cell.centre, 1.0, 1.0);
-		ctx.draw_ellipse(cell.centre, m_Grid.m_CellSize * 0.5f, m_Grid.m_CellSize * 0.5f);
+		ctx.fill_ellipse(cell->centre, 1.0, 1.0);
+		ctx.draw_ellipse(cell->centre, m_Grid.m_CellSize * 0.5f, m_Grid.m_CellSize * 0.5f);
 	}
 }
 
 void PathFindingGame::tick(double deltaTime)
 {
 	auto& input = GameEngine::instance()->get_input();
+	if(input->is_mouse_button_down(0))
+	{
+		int2 delta = input->get_mouse_delta();
+		m_ViewTranslation -= delta;
+	}
+
+	if(f32 scroll = input->get_scroll_delta(); scroll != 0.0f)
+	{
+		m_ViewZoom += scroll * 0.05f;
+	}
+
 	if (input->is_key_down(KeyCode::Right))
 	{
 		m_ViewTranslation.x += 10.0f;
@@ -125,49 +214,3 @@ void PathFindingGame::debug_ui()
 {
 }
 
-
-struct NavPathGrid
-{
-	struct Cell
-	{
-		// index to previously visited cell
-		u32 previous = -1;
-	};
-
-	std::vector<Cell> _cells;
-
-	Cell* get_cell(u32 idx)
-	{
-		return &_cells[idx];
-	}
-
-	static NavPathGrid from(NavGrid const& grid)
-	{
-		NavPathGrid result{};
-		result._cells.resize(grid.m_GridCells.size());
-
-		return result;
-	}
-
-};
-
-NavPathCell find_path(NavGrid& grid, u32 x0, u32 y0, u32 x1, u32 y1)
-{
-	NavPathGrid temp = NavPathGrid::from(grid);
-	u32 width = grid.m_Width;
-
-	u32 idx = x0 + y0 * width;
-
-	// Add start to frontier
-	std::vector<NavPathGrid::Cell*> frontier;
-	std::vector<NavPathGrid::Cell*> reached;
-	frontier.push_back(&temp._cells[idx]);
-
-	reached.push_back(&temp._cells[idx]);
-
-
-
-
-
-
-}
--- Source/PathfindingTest/PathFindingGame.h
+++ Source/PathfindingTest/PathFindingGame.h
@@ -4,8 +4,9 @@
 
 struct NavGridCell
 {
-	float2 centre = float2(0,0);
+	u32 idx = -1;
 
+	float2 centre = float2(0,0);
 	bool passable = true;
 };
 
@@ -20,22 +21,118 @@ struct NavGrid
 	std::vector<NavGridCell> m_GridCells;
 };
 
-struct NavPathCell
+inline NavGridCell* get_cell(NavGrid& grid, u32 x, u32 y)
 {
-	NavPathCell* previous;
+	if (x >= 0 && x < grid.m_Width && y >= 0 && y < grid.m_Height)
+	{
+		return &grid.m_GridCells[x + y * grid.m_Width];
+	}
+	return nullptr;
+}
+
+struct NavCell
+{
+	bool reached = false;
+
+	bool passable = true;
+
+	// index to previously visited cell
+	NavCell* previous = nullptr;
+
+	// Reference neighbouring cell indices
+	std::vector<NavCell*> neighbours;
 };
 
-NavPathCell find_path(NavGrid& grid, u32 x0, u32 y0, u32 x1, u32 y1)
+// Intermediate grid state. Each cell idx maps to an index in the original grid
+struct NavPathGrid
 {
+	// Cell storage
+	std::vector<NavCell> _cells;
 
+	NavCell* get_cell(u32 idx)
+	{
+		if (idx >= 0 && idx < _cells.size())
+			return &_cells[idx];
 
+		return nullptr;
+	}
 
-}
+	NavCell* get_cell(u32 x, u32 y)
+	{
+		if(x >= 0 && x < _width && y >= 0 && y < _height)
+		{
+			return &_cells[x + y * _width];
+		}
+		return nullptr;
+	}
 
-inline NavGridCell& get_cell(NavGrid& grid, u32 x, u32 y)
-{
-	return grid.m_GridCells[x + y * grid.m_Width];
-}
+	u32 get_cell_idx(NavCell* c) const
+	{
+		return u32(c - _cells.data());
+	}
+
+	NavCell* find_path(u32 x, u32 y)
+	{
+		NavCell* c = get_cell(x + y * _width);
+		if (c && c->previous != nullptr)
+		{
+			return c;
+		}
+
+		return nullptr;
+	}
+
+	void setup_links(NavGrid const& grid, bool diagonals = false)
+	{
+		// Hookup neighbours
+		for (u32 i = 0; i < _cells.size(); ++i)
+		{
+			u32 x = i % _width;
+			u32 y = i / _width;
+
+			NavCell& current = _cells[i];
+
+			// Copy from grid to nav cell
+			current.passable = grid.m_GridCells[i].passable;
+
+			// Horizontal
+			if (NavCell* c = get_cell(x- 1, y)) current.neighbours.push_back(c);
+			if (NavCell* c = get_cell(x+1,y))   current.neighbours.push_back(c);
+
+			// Vertical
+			if (NavCell* c = get_cell(x,y+1)) current.neighbours.push_back(c);
+			if (NavCell* c = get_cell(x, y-1)) current.neighbours.push_back(c);
+
+			// Diagonal
+			if(diagonals)
+			{
+				if (NavCell* c = get_cell(x- 1, y - 1)) current.neighbours.push_back(c);
+				if (NavCell* c = get_cell(x- 1, y + 1)) current.neighbours.push_back(c);
+
+				if (NavCell* c = get_cell(x + 1,y - 1 ))   current.neighbours.push_back(c);
+				if (NavCell* c = get_cell(x + 1,y + 1 ))   current.neighbours.push_back(c);
+			}
+		}
+	}
+
+	static NavPathGrid from(NavGrid const& grid)
+	{
+		NavPathGrid result{};
+		u32 size = grid.m_GridCells.size();
+
+		result._width = grid.m_Width;
+		result._height = grid.m_Height;
+		result._cells.resize(size);
+
+		result.setup_links(grid);
+
+		return result;
+	}
+
+	u32 _width;
+	u32 _height;
+};
+
 
 class PathFindingGame : public AbstractGame
 {
@@ -57,10 +154,12 @@ public:
 
 private:
 	float2 m_ViewTranslation;
+	f32 m_ViewZoom = 1.0f;
 
-	NavGridCell* m_Start;
-	NavGridCell* m_End;
+	uint2 m_Start;
+	uint2 m_End;
 
 	NavGrid m_Grid;
+	NavPathGrid m_NavigationGrid;
 
 };
